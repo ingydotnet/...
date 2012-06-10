9d196a85f70e6ba97e6d7e6a0d331e67fc64cae7
rking <rking@sharpsaw.org>
2012-06-10 06:24:39 -0600

    Getting rid of DOTDOTDOT_ROOT.
    It was problematic (chicken-and-egg issues all the time), plus it was
    needlessly indirect. `...` itself hard-codes $HOME/..., so the ~/.../lib
    should, too.
    
    If someone wants to install to something other than ~/..., they can set
    HOME=somethingelse.

--- lib/path-manip.sh
+++ lib/path-manip.sh
@@ -1,6 +1,6 @@
 # (Here for backwards compatibility. This is not actually a /bin/sh script, so
 # the .sh name isn't quite right.)
-source $DOTDOTDOT_ROOT/lib/path-manip
+source ~/.../lib/path-manip
 alias path-list=...path-list
 alias path-remove=...path-remove
 alias path-prepend=...path-prepend
