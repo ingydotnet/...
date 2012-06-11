deeae4d1e87543ea338eab24082a76ed6dd14ad3
Ingy döt Net <ingy@ingy.net>
2012-06-11 09:32:53 -0600

    Fail better when run under /bin/sh.

--- lib/path-manip.sh
+++ lib/path-manip.sh
@@ -1,5 +1,8 @@
 # (Here for backwards compatibility. This is not actually a /bin/sh script, so
 # the .sh name isn't quite right.)
+
+source ~/.../lib/assert-decent-shell || return
+
 source ~/.../lib/path-manip
 alias path-list=...path-list
 alias path-remove=...path-remove
