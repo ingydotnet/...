ffc1b9e68fa1f06a56c03f42459faafbedde210d
Ingy döt Net <ingy@ingy.net>
2012-06-12 19:05:12 +0000

    Slightly better as functions.

--- lib/path-manip.sh
+++ lib/path-manip.sh
@@ -1,7 +1,15 @@
 # (Here for backwards compatibility. This is not actually a /bin/sh script, so
 # the .sh name isn't quite right.)
 source $DOTDOTDOT_ROOT/lib/path-manip
-alias path-list=...path-list
-alias path-remove=...path-remove
-alias path-prepend=...path-prepend
-alias path-append=...path-append
+path-list() {
+    ...path-list "$@"
+}
+path-remove() {
+    ...path-remove "$@"
+}
+path-prepend() {
+    ...path-prepend "$@"
+}
+path-append() {
+    ...path-append "$@"
+}
